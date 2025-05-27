37f0f09b31daae5cad7f954653edf7da65e15893
User-2025MMLEKR\Administrator <850019462@qq.com>
2025-05-27 10:05:04 +0800

    移除无用包

--- src/test/java/top/keir/mybatis/Demo.java
+++ src/test/java/top/keir/mybatis/Demo.java
@@ -14,16 +14,7 @@ public class Demo {
 
     @Test
     public void test() {
-        Set<Integer> list0 = Set.of(1, 2, 3, 4, 5);
-        Set<Integer> list1 = Set.of(1, 2, 3, 4, 5);
-        System.out.println("list0.equals(list1) = " + list0.equals(list1));
-        Set<Integer> list2 = Set.of(1, 2, 3, 4, 6);
-        // 新增
-        Set<Integer> addSet = list2.stream().filter(item -> !list0.contains(item)).collect(Collectors.toSet());
-        // 删除
-        Set<Integer> delSet = list0.stream().filter(item -> !list2.contains(item)).collect(Collectors.toSet());
-        log.info("addSet = {}, delSet = {}", addSet, delSet);
-
+        System.out.println("System.getenv(\"JAVA_HOME\") = " + System.getenv("JAVA_HOME"));
     }
 
 }
